cdfe0285e58d85a1a886f80e4e64940f20a04b32
David Buser <busdav@outlook.com>
2019-12-01 12:50:28 +1300

    Start implement authentication

---
+++ middleware/auth.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+// We did this manually instead of using Passport, as Passport is somewhat heavy and overkill for this project. 
+// I might still use Passport though
+
+module.exports = function(req, res, next) {
+  // Get token from header
+  const token = req.header("x-auth-token");
+
+  // Check if no token
+  if(!token) {
+    return res.status(401).json({ msg: "No token, authorization denied" });
+  }
+
+  // If there is token, verify it
+  try {
+    const decoded = jwt.verify(token, config.get("jwtSecret"));
+    
+    req.user = decoded.user;
+    next();
+  } catch(err) {
+    res.status(401).json({ msg: "Token is not valid" });
+  }
+}
