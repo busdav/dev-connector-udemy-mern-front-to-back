c29b2bb7ed3d1f2978e9821a3cb4e8820b9293c3
David Buser <busdav@outlook.com>
2019-12-05 19:21:27 +1300

    Complete create profile method

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -58,6 +58,48 @@ async (req, res) => {
 
   // Build profile object
   const profileFields = {};
+  profileFields.user = req.user.id;
+  if (company) profileFields.company = company;
+  if (website) profileFields.website = website;
+  if (location) profileFields.location = location;
+  if (bio) profileFields.bio = bio;
+  if (status) profileFields.status = status;
+  if (githubusername) profileFields.githubusername = githubusername;
+  if(skills) {
+    profileFields.skills = skills.split(",").map(skill => skill.trim());
+  }
+
+  // Build social object (part of the profile object)
+  profileFields.social = {};
+  if (youtube) profileFields.social.youtube = youtube;
+  if (twitter) profileFields.social.twitter = twitter;
+  if (facebook) profileFields.social.facebook = facebook;
+  if (linkedin) profileFields.social.linkedin = linkedin;
+  if (instagram) profileFields.social.instagram = instagram;
+
+  try {
+    let profile = await Profile.findOne({ user: req.user.id });
+    
+    if(profile) {
+      // Update profile - he does that so he can use one route for create and update - alternatives: two separate routes, or mongoose's `upsert` option
+      profile = await Profile.findOneAndUpdate(
+        { user: req.user.id },
+        { $set: profileFields },
+        { new: true, useFindAndModify: false }
+      );
+
+      return res.json(profile);
+    }
+
+    // Create
+    profile = new Profile(profileFields);
+    await profile.save();
+    res.json(profile);
+
+  } catch(err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
 
 });
 
