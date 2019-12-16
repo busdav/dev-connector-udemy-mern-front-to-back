9f1dd82f6ef6230d42984fb32964b68dbc0e0af0
David Buser <busdav@outlook.com>
2019-12-16 08:56:57 +0000

    Add login and register components

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,26 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
 import './App.css';
 
 const App = () => (
+  <BrowserRouter>
     <Fragment>
       <Navbar />
-      <Landing />
+      <Route exact path='/' component={Landing} /> 
+      <section className="container"> 
+        <Switch>
+          <Route exact path='/register' component={Register} />
+          <Route exact path='/login' component={Login} />
+        </Switch>
+      </section>
     </Fragment>
+  </BrowserRouter>
 );
+
+// `container` above because that one centers everything (we only don't want it for the landing page with the image)
 
 export default App;
