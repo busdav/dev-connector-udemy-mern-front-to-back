5485e7bc54522c94c2a5b46fad8bf719285866de
David Buser <busdav@outlook.com>
2019-12-30 14:06:03 +0100

    Implement  REMOVE_ALERT action type

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -5,10 +5,12 @@ import { SET_ALERT, REMOVE_ALERT } from './types';
 We want to be able to dispatch more than one action type from the funtion below -> we can do so by using the thunk middleware, 
 which allows us to state `=> dispatch => {}`
 */
-export const setAlert = (msg, alertType) => dispatch => {
+export const setAlert = (msg, alertType, timeout = 5000) => dispatch => {
   const id = uuid.v4();
   dispatch({
     type: SET_ALERT,
     payload: { msg, alertType, id }
   });
+
+  setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), timeout);
 };
